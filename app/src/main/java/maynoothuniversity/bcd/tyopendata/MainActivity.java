8a7890b27f8505fe074bb805f54b9e89190942ba
dawnithan <templer-vvv@live.ie>
2018-04-27 17:37:13 +0100

    Implement clustering, camera/zoom/boundary settings

--- app/src/main/java/maynoothuniversity/bcd/tyopendata/MainActivity.java
+++ app/src/main/java/maynoothuniversity/bcd/tyopendata/MainActivity.java
@@ -1,34 +1,53 @@
 package maynoothuniversity.bcd.tyopendata;
 
-import android.os.AsyncTask;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.gson.JsonObject;
-import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.IconFactory;
 import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.geometry.LatLngBounds;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.plugins.cluster.clustering.ClusterItem;
+import com.mapbox.mapboxsdk.plugins.cluster.clustering.ClusterManagerPlugin;
 import com.mapbox.mapboxsdk.plugins.geojson.GeoJsonPlugin;
 import com.mapbox.mapboxsdk.plugins.geojson.GeoJsonPluginBuilder;
 import com.mapbox.mapboxsdk.plugins.geojson.listener.OnLoadingGeoJsonListener;
 import com.mapbox.mapboxsdk.plugins.geojson.listener.OnMarkerEventListener;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, OnLoadingGeoJsonListener, OnMarkerEventListener {
 
+    // Bounding box
+    private static final LatLngBounds BALLYFERMOT = new LatLngBounds.Builder()
+            .include(new LatLng(53.401447, -6.400051))
+            .include(new LatLng(53.281997, -6.297312))
+            .build();
+
     private MapView mapView;
     private MapboxMap mapboxMap;
-    private GeoJsonPlugin geoJsonPlugin;
+    private ClusterManagerPlugin<MyItem> mClusterManagerPlugin;
+
+    // IconFactory iconFactory = IconFactory.getInstance(MainActivity.this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,34 +65,135 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(MapboxMap mapboxMap) {
         this.mapboxMap = mapboxMap;
 
-        geoJsonPlugin = new GeoJsonPluginBuilder()
-                .withContext(this)
-                .withMap(mapboxMap)
-                .withOnLoadingURL(this)
-                .withOnLoadingFileAssets(this)
-                .withOnLoadingFilePath(this)
-                .withMarkerClickListener(this)
-                .build();
+        mapboxMap.setLatLngBoundsForCameraTarget(BALLYFERMOT);
+        mapboxMap.setMaxZoomPreference(18);
+        mapboxMap.setMinZoomPreference(10);
 
-        geoJsonPlugin.setAssetsName("education_sample.geojson");
+        mClusterManagerPlugin = new ClusterManagerPlugin<>(this, mapboxMap);
+        doCluster();
+
+//        GeoJsonPlugin geoJsonPlugin = new GeoJsonPluginBuilder()
+//                .withContext(this)
+//                .withMap(mapboxMap)
+//                .withOnLoadingURL(this)
+//                .withOnLoadingFileAssets(this)
+//                .withOnLoadingFilePath(this)
+//                .withMarkerClickListener(this)
+//                .build();
+//
+//        geoJsonPlugin.setAssetsName("education_sample.geojson");
     }
 
     @Override
     public void onMarkerClickListener(Marker marker, JsonObject properties) {
-        String name = properties.get("Name").toString();
-        String address = properties.get("Address").toString();
-        String phone = properties.get("Phone ").toString();
-        String website = properties.get("Website ").toString();
-        String email = properties.get("Email").toString();
+//        String name = properties.get("Name").toString();
+//        String address = properties.get("Address").toString();
+//        String phone = properties.get("Phone ").toString();
+//        String website = properties.get("Website ").toString();
+//        String email = properties.get("Email").toString();
+//
+//        name = name.replace('"',' ');
+//        address = address.replace('"',' ');
+//        phone = phone.replace('"',' ');
+//        website = website.replace('"',' ');
+//        email = email.replace('"',' ');
+//
+//        marker.setTitle(name);
+//        marker.setSnippet(address + "\n" + phone + "\n" + website + "\n" + email);
+    }
 
-        name = name.replace('"',' ');
-        address = address.replace('"',' ');
-        phone = phone.replace('"',' ');
-        website = website.replace('"',' ');
-        email = email.replace('"',' ');
+    protected void doCluster() {
+        mapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(53.342315,-6.354475), 14));
+        mapboxMap.addOnCameraIdleListener(mClusterManagerPlugin);
+        try {
+            readItems();
+        } catch (JSONException exception) {
+            Toast.makeText(this, "Problem reading list of markers.", Toast.LENGTH_LONG).show();
+            //Log.d("Cluster Problem: ", exception.toString());
+        }
+    }
 
-        marker.setTitle(name);
-        marker.setSnippet(address + "\n" + phone + "\n" + website + "\n" + email);
+    private void readItems() throws JSONException {
+        InputStream inputStream = getResources().openRawResource(R.raw.education_sample_javascript);
+        List<MyItem> items = new MyItemReader().read(inputStream);
+        mClusterManagerPlugin.addItems(items);
+    }
+
+    public static class MyItem implements ClusterItem {
+        private final LatLng mPosition;
+        private String mTitle;
+        private String mSnippet;
+
+        public MyItem(double lat, double lng) {
+            mPosition = new LatLng(lat, lng);
+            mTitle = null;
+            mSnippet = null;
+        }
+
+        private MyItem(double lat, double lng, String title, String snippet) {
+            mPosition = new LatLng(lat, lng);
+            mTitle = title;
+            mSnippet = snippet;
+        }
+
+        @Override
+        public LatLng getPosition() {
+            return mPosition;
+        }
+
+        @Override
+        public String getTitle() {
+            return mTitle;
+        }
+
+        @Override
+        public String getSnippet() {
+            return mSnippet;
+        }
+
+        public void setTitle(String title) {
+            mTitle = title;
+        }
+
+        public void setSnippet(String snippet) {
+            mSnippet = snippet;
+        }
+    }
+
+    private static class MyItemReader {
+
+        private static final String REGEX_INPUT_BOUNDARY_BEGINNING = "\\A";
+
+
+        private List<MyItem> read(InputStream inputStream) throws JSONException {
+            List<MyItem> items = new ArrayList<>();
+            String json = new Scanner(inputStream).useDelimiter(REGEX_INPUT_BOUNDARY_BEGINNING).next();
+            JSONArray array = new JSONArray(json);
+            for (int i = 0; i < array.length(); i++) {
+                String title = "";
+                String snippet = "";
+                JSONObject object = array.getJSONObject(i);
+                double lat = object.getDouble("Latitiude"); // lol
+                double lng = object.getDouble("Longitude");
+                if (!object.isNull("Name")) {
+                    title = object.getString("Name");
+                }
+                if (!object.isNull("Address")) {
+                    snippet += object.getString("Address") + "\n";
+                }
+                if (!object.isNull("Phone")) {
+                    snippet += object.getString("Phone") + "\n";
+                }
+                if (!object.isNull("Website")) {
+                    snippet += object.getString("Website") + "\n";
+                }
+                if (!object.isNull("Email")) {
+                    snippet += object.getString("Email");
+                }
+                items.add(new MyItem(lat, lng, title, snippet));
+            }
+            return items;
+        }
     }
 
     @Override
---
+++ app/src/main/res/raw/education_sample_javascript.js
@@ -0,0 +1,86 @@
+[
+ {
+   "Name": "St Dominics College",
+   "Address": "Kylemore Rd. Chapelizod",
+   "Town": "Ballyfermot",
+   "County": "Dublin",
+   "Postcode": "Dublin 10",
+   "Latitiude": 53.3438017,
+   "Longitude": -6.3469855,
+   "Phone": "00353 1 626 6493",
+   "Website": "www.stdominicsballyfermot.ie",
+   "Email": "info@stdominicsballyfermot.ie"
+ },
+ {
+   "Name": "Kylemore College",
+   "Address": "Kylemore Rd. Chapelizod",
+   "Town": "Ballyfermot",
+   "County": "Dublin",
+   "Postcode": "Dublin 10",
+   "Latitiude": 53.3452464,
+   "Longitude": -6.3516211,
+   "Phone": "00353 1 626 5901",
+   "Website": "www.kylemorecollege.ie",
+   "Email": "info@kylemore.cdetb.ie"
+ },
+ {
+   "Name": "Caritas College",
+   "Address": "10 Drumfinn Rd. Drumfinn",
+   "Town": "Ballyfermot",
+   "County": "Dublin",
+   "Postcode": "Dublin 10",
+   "Latitiude": 53.3478168,
+   "Longitude": -6.3598718,
+   "Phone": "00353 1 626 5927",
+   "Website": "www.caritascollege.ie",
+   "Email": "info@caritascollege.ie"
+ },
+ {
+   "Name": "St Johns College",
+   "Address": "10 Le Fanu Rd. Drumfinn",
+   "Town": "Ballyfermot",
+   "County": "Dublin",
+   "Postcode": "Dublin 10",
+   "Latitiude": 53.3445097,
+   "Longitude": -6.3552197,
+   "Phone": "00353 1 626 4943",
+   "Website": "www.stjohnsdls.ie",
+   "Email": " info@stjohnscollege.ie"
+ },
+ {
+   "Name": "BCFE Main Building",
+   "Address": "269 Ballyfermot Rd. Kylemore",
+   "Town": "Ballyfermot",
+   "County": "Dublin",
+   "Postcode": "Dublin 10",
+   "Latitiude": 53.3405489,
+   "Longitude": -6.352854,
+   "Phone": "00353 1 626 9421",
+   "Website": "www.bcfe.ie",
+   "Email": " info@bcfe.cdetb.ie"
+ },
+ {
+   "Name": "BCFE Arts Block",
+   "Address": "33 Convent Lawns. Chapelizod",
+   "Town": "Ballyfermot",
+   "County": "Dublin",
+   "Postcode": "Dublin 10",
+   "Latitiude": 53.3450253,
+   "Longitude": -6.3499771,
+   "Phone": "00353 1 6269421",
+   "Website": "www.bcfe.ie",
+   "Email": "info@bcfe.cdetb.ie"
+ },
+ {
+   "Name": "Dublin College of Hairdressing",
+   "Address": "34-39 Gurteen Road. Redcowfarm",
+   "Town": "Ballyfermot",
+   "County": "Dublin",
+   "Postcode": "Dublin 10",
+   "Latitiude": 53.3458824,
+   "Longitude": -6.3624512,
+   "Phone": "00353 85 724 9647",
+   "Website": "www.irishcollegeofhairdressing.com",
+   "Email": ""
+ }
+]
